f1b9554d10c89e51bc554de1be9fb6534cd860ee
Gabriel <pucikplay@gmail.com>
2023-03-12 13:42:11 +0100

    Graphs continuation

--- Labo/L1/z1_graphs.py
+++ Labo/L1/z1_graphs.py
@@ -9,5 +9,16 @@ for i, line in enumerate(data):
     data_arr.append(line[:-2].split(","))
 
 n_vals = [100,500,1000,5000,10000,50000,100000]
+distributions = ["getUniform", "getHarmonic", "getBiharmonic", "getGeometric"]
+self_orders = ["nothing", "moveToFront", "transpose", "count"]
+all = [[None]] * 16
 
-# for i, n in enumerate(n_vals):
+for i, n in enumerate(n_vals):
+    for j in range(0, len(data_arr[i])):
+        if all[j] == [None]:
+            all[j] = [data_arr[i][j]]
+        else:
+            all[j].append(data_arr[i][j])
+
+for dist in distributions:
+    for order in self_orders:
